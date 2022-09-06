c1963ac14a70001f317b53316aabee215d03fbb6
Tommy René Sætre <tommy.saetre@hotmail.com>
2022-09-06 08:16:12 +0200

    Optimized MNIST model, need further work

--- assignment-2/assignment_2_d.py
+++ assignment-2/assignment_2_d.py
@@ -6,21 +6,18 @@ from tqdm import tqdm
 
 # matplotlib.use('WebAgg')
 
-EPOCHS = 100000
-LEARNING_RATE = 0.1
+EPOCHS = 100
+LEARNING_RATE = 1
 DATASET_SIZE = 784
 
-mnist_transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Normalize([0.], [1.])])
-mnist_train = torchvision.datasets.MNIST('./data', train=True, download=True, transform=mnist_transform)
-train_x = mnist_train.data.reshape(-1, DATASET_SIZE).float()
+mnist_train = torchvision.datasets.MNIST('./data', train=True, download=True)
+train_x = mnist_train.data.reshape(-1, DATASET_SIZE).float() / 255.0
 train_y = torch.zeros((mnist_train.targets.shape[0], 10))
 train_y[torch.arange(mnist_train.targets.shape[0]), mnist_train.targets] = 1
 
-print(train_x[0])
-
 class NeuralMNIST:
     def __init__(self):
-        self.W = torch.zeros((DATASET_SIZE, 28), requires_grad=True)
+        self.W = torch.zeros((DATASET_SIZE, 10), requires_grad=True)
         self.b = torch.tensor([[0.0]], requires_grad=True)
 
     def logits(self, x):
@@ -45,13 +42,20 @@ for epoch in tqdm(range(EPOCHS)):
 
     optimizer.zero_grad()
 
+print('W = %s, b = %s, loss = %s' % (model.W, model.b, model.loss(train_x, train_y)))
+
 mnist_test = torchvision.datasets.MNIST('./data', train=False, download=True)
-test_x = mnist_test.data.reshape(-1, 784).float()
+test_x = mnist_test.data.reshape(-1, DATASET_SIZE).float() / 255.0
 test_y = torch.zeros((mnist_test.targets.shape[0], 10))
 test_y[torch.arange(mnist_test.targets.shape[0]), mnist_test.targets] = 1
 
-plt.imshow(train_x[0, :].reshape(28, 28))
+predicted = model.f(test_x)
+i = 0
+for tensor in predicted:
+    if i < 20:
+        print("Actual: %s\nPredicted: %s\n" % (test_y[i], predicted))
+    i += 1
 
-print(train_y[0, :])
+plt.imshow(train_x[0, :].reshape(28, 28))
 
 plt.show()
